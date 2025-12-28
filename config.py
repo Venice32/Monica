89d73f81cd3f1bad103146ac02f7cd91de0120c9
Venice32 <pipoto963@gmail.com>
2025-12-28 05:37:34 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,1 @@
-# config.py
-# Fichier de configuration pour Monica
 
-# --- API EXTERNES ---
-# Pour la météo, inscrivez-vous sur https://openweathermap.org/api pour obtenir une clé gratuite.
-CLE_API_METEO = "VOTRE_CLÉ_API_OPENWEATHERMAP"  # Remplacez par votre clé
-
-# --- PARAMÈTRES UTILISATEUR ---
-VILLE_PAR_DEFAUT = "Paris"
-LANGUE_PAR_DEFAUT = "fr"
-
-# --- AUTRES ---
-FICHIER_RAPPELS = "rappels.json"
